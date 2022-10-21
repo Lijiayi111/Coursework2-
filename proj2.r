c543f569aec1ffd955019ea8e8b82aa8fcb3486a
Lijiayi111 <113895450+Lijiayi111@users.noreply.github.com>
2022-10-21 09:15:14 +0100

    Update proj2.r

--- proj2.r
+++ proj2.r
@@ -113,135 +113,67 @@ Pall(50,3,10000)
 
 
 
-function66 <- function(x,n){
-  t <- rep(0,2*n)
-  length<- rep(0,2*n)
-  for (k in 1:(2*n)){ 
-    b <- k
-    t[k]<- k
-    print("tvalue is ")
-    print(t)
-    card<-rep(0,2*n)
-    
-    # construct the card list we will take  
-    for (i in 1:(2*n)){
-       card[i]<-x[b]
-       b<-x[b]}
-    print(card)
-    # the index of card when k first equals to card 
-    length[k] <- match(k,card)
-    print(length[k])
-  }
-  
-  print(length)
-  
-  return(length)
-}
-
-
-dloop<- function(n,nreps){
-  SamMatrix<-matrix(nrow=nreps,ncol=2*n)
-  # interate the function66 by nreps time by creating nreps row of random cards
+## To calculate two probability at the same time. one is the at least one time occuring probability called result 1, second is 
+## the probability of all less than 50 called result two.
+dloop <- function(n, nreps) {
+  # the probability matrix before processing which will accomodate the at least one time occuring probability
+  preProbMatrix <- matrix(0, nrow = nreps, ncol = 2 * n)
+  # initilized the number of time of all less than n
+  count <- 0
+  # the interation of nreps times
   for (i in 1:nreps) {
-    SamMatrix[i,]<-sample(1:(2*n),2*n)
-  }
-  # apply the function 66 for every row 
-  result<-apply(SamMatrix, 1, function66,n=n)
-  #print(result)
-  
-  print(result)
-  print(table(result))
-  result66 <- table(result)/nreps
-  
-  return(result66)
-}
-
-
-
-
-
-
-# Question 5
-dloop <- function(n,nreps){
-   # the probability matrix before processing which will accomodate the at least one time occuring probability 
-   preProbMatrix <- matrix(0,nrow =nreps, ncol = 2*n) 
-   # the interation of nreps times 
-   for (i in 1:nreps){
-     # the cards behind the box are uniformally random number
-     card <- sample(1:(2*n),2*n)
-     # initilized the number of time of all less than n 
-     count <- 0 
-     for (k in 1:(2*n)){
-       # initilize the loop length
-       loopLength <- 1
-       # k denoted the person's index 
-       ## initialize the first box choosen which is equal to the prisoner number
-       boxChoosen <- k
-       while (loopLength <= 2*n){
-         if(card[boxChoosen] == k){
-           #  when at least a length occure just record as 1
-           preProbMatrix[i,loopLength] <- 1
-           break # immidiatly go to check next prisoner, jump out of the while loop
-         }else{
-           # turn to choose the next box
-           boxChoosen <- card[boxChoosen]
-           # length increase by 1
-           loopLength  <- loopLength  + 1
-         }
-       }
-     }
-     # during every interation when all less than 50 interation happens, counter increase by 1.
-     if(sum(preProbMatrix[i, (n+1):(2*n)]) == 0){
-       count <- count + 1 # count increase by 1 
-     }
-   # sum all the column and divide by total number of column
-   prob <- colSums(preProbMatrix)/nreps  
-   # the probability of all less than 50
-   prolessthann <- count/nreps   
-   print(prolessthann)
-   result <- list()
-   result[[1]] <- prob
-   result[[2]] <- prolessthann
-   return(result)
-}
-}
-
-loopLength <- 1:100
-result <- dloop(50, 10000)
-
-
-plot(loopLength, prob, xlab = "Length of loop", ylab = "Probability of loop",
-     main = "Probabilities v.s. length of loop")
-
-# Question 6
-dlooplowerthan50 <- function(n, nreps){
-  preProbMatrix <- matrix(0, nrow = nreps, ncol = 2*n)
-  count <- 0 # count the number of iteration satisfied our demand 
-
-}
-dloop1 <- function(n, nreps){#与第五题大致相同，主要用于统计循环长度小于等于50的情况，任何大于50的情况都不考虑
-  prob_matrix <- matrix(0, nrow = nreps, ncol = 2*n) #概率矩阵
-  box <- 1:(2*n) #盒子
-  b <- 0 #循环长度小于等于50的计数器
-  for (i in 1:nreps) {
-    ncard <- sample(1:(2*n), 2*n) #随机洗牌
-    for (j in 1:(2*n)) {
-      box <- j
-      depth <- 1
-      while (depth <= 2*n) {
-        if(ncard[box] == j){
-          prob_matrix[i, depth] = 1
-          break
-        }else{
-          box = ncard[box]
-          depth = depth + 1
+    # the cards behind the box are uniformally random number
+    card <- sample(1:(2 * n), 2 * n)
+    for (k in 1:(2 * n)) {
+      # initilize the loop length
+      loopLength <- 1
+      # k denoted the person's index
+      ## initialize the first box choosen which is equal to the prisoner number
+      boxChoosen <- k
+      while (loopLength <= 2 * n) {
+        if (card[boxChoosen] == k) {
+          #  when at least a length occure just record as 1
+          preProbMatrix[i, loopLength] <- 1
+          break # immidiatly go to check next prisoner, jump out of the while loop
+        } else{
+          # turn to choose the next box
+          boxChoosen <- card[boxChoosen]
+          # length increase by 1
+          loopLength  <- loopLength  + 1
         }
       }
     }
-    if(sum(prob_matrix[i, (n+1):(2*n)]) == 0){ #51-100全为0时
-      b <- b + 1 #计数器+1
+    # during every interation when all less than 50 interation happens, counter increase by 1.
+    if (sum(preProbMatrix[i, (n + 1):(2 * n)]) == 0) {
+      count <- count + 1 # count increase by 1
     }
   }
-  return(b/nreps)
+  
+  # sum all the column and divide by total number of column
+  prob <- colSums(preProbMatrix) / nreps
+  # the probability of all less than 50
+  prolessthann <- count / nreps
+  result <- list()
+  # make them alist to return them all
+  result[[1]] <- prob
+  result[[2]] <- prolessthann
+  return(result)
+  
 }
-dloop1(50, 10000)
+
+# return the two probability by calling the dloop function 
+result <- dloop(50, 10000)
+
+#the at least one time occuring probability
+prob <- result[[1]]
+# the probability of all less than 50
+prolessthann <- result[[2]]
+
+# make the x-axis form 1 to 2n
+loopLength <- 1:100
+# visualising the probabilities
+plot(loopLength,
+     prob,
+     xlab = "Length of loop",
+     ylab = "Probability of loop",
+     main = "Probabilities v.s. length of loop")
